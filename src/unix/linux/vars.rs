93716fdada165cbd4e9f385ce0de6b0cd5e2916b
Hentioe <meow.i5.br@gmail.com>
2024-03-25 07:22:36 +0800

    Rename external root var

--- src/unix/linux/vars.rs
+++ src/unix/linux/vars.rs
@@ -1,10 +1,10 @@
 use lazy_static::lazy_static;
 
 lazy_static! {
-    pub static ref CGS_AGENT_EXTERNAL_ROOT: String =
-        std::env::var("CGS_AGENT_EXTERNAL_ROOT").unwrap_or_else(|_| String::new());
+    pub static ref SPROUT_AGENT_EXTERNAL_ROOT: String =
+        std::env::var("SPROUT_AGENT_EXTERNAL_ROOT").unwrap_or_else(|_| String::new());
 }
 
 pub fn root_join(path: &str) -> String {
-    format!("{}{}", *CGS_AGENT_EXTERNAL_ROOT, path)
+    format!("{}{}", *SPROUT_AGENT_EXTERNAL_ROOT, path)
 }
